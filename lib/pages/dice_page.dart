1d69bd0c9727dcf0c81d63929a740d7cb996e42c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-05-25 10:38:24 -0500

    dice logic

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int n = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +15,7 @@ class DicePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -27,8 +34,41 @@ class DicePage extends StatelessWidget {
               // color: Colors.red,
               width: 200,
               height: 200,
-              child: Image.asset("assets/images/dice1.png"),
+              child: Image.asset("assets/images/dice$n.png"),
             ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (n == 1) {
+                      n = 6;
+                    } else {
+                      n--;
+                    }
+                    setState(() {});
+                  },
+                  child: Icon(Icons.arrow_back_ios),
+                ),
+                SizedBox(
+                  width: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (n == 6) {
+                      n = 1;
+                    } else {
+                      n++;
+                    }
+                    setState(() {});
+                  },
+                  child: Icon(Icons.arrow_forward_ios),
+                ),
+              ],
+            )
           ],
         ),
       ),
