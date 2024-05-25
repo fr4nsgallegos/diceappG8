1b3ded02232901e1b3c1dd264cf7ff877c446d4d
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-05-25 10:04:24 -0500

    border radius circular Container

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -7,6 +7,31 @@ class DicePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Dice App"),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                //PRIMERA FORMA DE APLICAR BORDE CIRCULAR A MI CONTAINER
+                borderRadius: BorderRadius.circular(20),
+                // borderRadius: BorderRadius.only(
+                //   topRight: Radius.circular(20),
+                //   topLeft: Radius.circular(50),
+                // ),
+
+                //SEGUNDA FORMA (En ésta se tranforma en un círculo)
+                // shape: BoxShape.circle,
+              ),
+              padding: EdgeInsets.all(16),
+              // color: Colors.red,
+              width: 200,
+              height: 200,
+              child: Image.asset("assets/images/dice1.png"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
